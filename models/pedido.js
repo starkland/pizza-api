b4f8610d46c4498ae572b8f84574c756fb14c323
thulioph <thulioph@gmail.com>
2017-02-07 01:17:21 -0300

    Ajusta o schema de pedido.

--- models/pedido.js
+++ models/pedido.js
@@ -10,7 +10,37 @@ const PedidoSchema = new Schema({
   name: {
     type: String,
     default: '',
-    // required: true
+    required: true
+  },
+
+  email: {
+    type: String,
+    required: true
+  },
+
+  endereco: {
+    type: String,
+    required: true
+  },
+
+  cep: {
+    type: String,
+    required: true
+  },
+
+  telefone: {
+    type: String,
+    required: true
+  },
+
+  tamanho: {
+    type: String,
+    required: true
+  },
+
+  ingredientes: {
+    type: Array,
+    required: true
   },
 
   created_at: {
