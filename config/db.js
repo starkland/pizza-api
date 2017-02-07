300dd1b1f941515d8aed3b23be5b8658399f9e12
thulioph <thulioph@gmail.com>
2017-02-07 01:01:50 -0300

    Adiciona arquivo de configuração do mongo.

---
+++ config/db.js
@@ -0,0 +1,40 @@
+let mongoose = require('mongoose');
+
+// ====
+
+mongoose.connect('mongodb://localhost/laricas');
+
+// ====
+
+const db = mongoose.connection;
+
+// ====
+
+db.on('error', function(err) {
+  console.log(`Erro de conexão: ${err}`);
+});
+
+db.on('open', function() {
+  console.log('Conexão aberta!');
+});
+
+db.on('connected', function() {
+  console.log('Conectado!');
+});
+
+db.on('disconnected', function() {
+  console.log('Desconectado!');
+});
+
+// ====
+
+process.on('SIGINT', function() {
+  mongoose.connection.close(function() {
+    console.log('A conexão com o mongoose foi encerrada.');
+    process.exit(0);
+  });
+});
+
+// ====
+
+require('../models/pedido.js');
